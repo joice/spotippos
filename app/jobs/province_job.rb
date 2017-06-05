952d30e834b1d7eaf0a742b0564acae39e309d00
Joice Taciana <joicetaciana@gmail.com>
2017-06-05 00:25:47 -0300

    Improve sql injection

--- app/jobs/province_job.rb
+++ app/jobs/province_job.rb
@@ -5,7 +5,15 @@ class ProvinceJob < ApplicationJob
   def perform(args)
     property = Property.find(args[:property_id])
 
-    province_ids = Property.find_by_sql(['SELECT properties.id, array_agg(provinces.id order by provinces.id) as area_list from provinces, properties where st_contains(provinces.area, properties.lonlat) and properties.id = ? group by properties.id order by properties.id', property.id]).collect(&:area_list).flatten
+    query = <<-SQL
+      SELECT properties.id, array_agg(provinces.id order by provinces.id) AS area_list
+      FROM provinces, properties
+      WHERE st_contains(provinces.area, properties.lonlat) AND properties.id = ?
+      GROUP BY properties.id
+      ORDER BY properties.id
+    SQL
+
+    province_ids = Property.find_by_sql([query, property.id]).collect(&:area_list).flatten
 
     property.province_ids = province_ids
   end
