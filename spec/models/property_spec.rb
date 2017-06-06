5d96a5e5bcf845401bfbd0bb24b8b3c2c01f26e3
Joice Taciana <joicetaciana@gmail.com>
2017-06-05 21:41:13 -0300

    Add unitary test for find_by_polygon method

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -9,6 +9,41 @@ RSpec.describe Property, type: :model do
     it { is_expected.to validate_numericality_of(:square_meters).only_integer.is_less_than_or_equal_to(240).is_greater_than_or_equal_to(20) }
   end
 
+  describe '.find_by_polygon' do
+    let!(:property_1) { create(:property, x: 1257, y: 928) }
+    let!(:property_2) { create(:property, x: 679,  y: 680) }
+    let!(:property_3) { create(:property, x: 1051, y: 441) }
+    let!(:property_4) { create(:property, x: 252,  y: 868) }
+    let!(:property_5) { create(:property, x: 34,   y: 660) }
+    let!(:property_6) { create(:property, x: 1363, y: 122) }
+    let!(:property_7) { create(:property, x: 38,   y: 664) }
+    let!(:property_8) { create(:property, x: 1201, y: 592) }
+    let!(:property_9) { create(:property, x: 795,  y: 534) }
+    let!(:property_10) { create(:property, x: 304, y: 225) }
+
+    context 'finds properties that are within a polygon' do
+      let!(:gode_params) { { ax: 0, ay: 1000, bx: 600, by: 500 } } # Gode boundaries
+      let!(:ruja_params) { { ax: 400, ay: 1000, bx: 1100, by: 500 } } # Ruja boundaries
+
+      it { expect(Property.find_by_polygon(gode_params).size).to eq(3) }
+      it { expect(Property.find_by_polygon(gode_params)).to match_array([property_4, property_5, property_7]) }
+
+      it { expect(Property.find_by_polygon(ruja_params).size).to eq(2) }
+      it { expect(Property.find_by_polygon(ruja_params)).to match_array([property_2, property_9]) }
+    end
+
+    context 'finds properties that are within a polygon and paginate them' do
+      let!(:gode_params) { { ax: 0, ay: 1000, bx: 600, by: 500, per_page: 1 } } # Gode boundaries
+      let!(:ruja_params) { { ax: 400, ay: 1000, bx: 1100, by: 500, per_page: 1, page: 2 } } # Ruja boundaries
+
+      it { expect(Property.find_by_polygon(gode_params).size).to eq(1) }
+      it { expect(Property.find_by_polygon(gode_params)).to match_array(property_4) }
+
+      it { expect(Property.find_by_polygon(ruja_params).size).to eq(1) }
+      it { expect(Property.find_by_polygon(ruja_params)).to match_array(property_9) }
+    end
+  end
+
   describe 'callbacks' do
     context '#set_lonlat!' do
       let(:property) { build :property }
